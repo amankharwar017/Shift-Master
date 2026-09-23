878f34bee3887560d5b9a3d0956e3413d39fda1c
Aman Kharwar <amankharwar755@gmail.com>
2026-09-23 23:18:26 +0530

    Initial Shift Master project setup

---
+++ src/main/java/com/shiftmaster/ShiftMasterApplication.java
@@ -0,0 +1,13 @@
+package com.shiftmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiftMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShiftMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shiftmaster/ShiftMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shiftmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
